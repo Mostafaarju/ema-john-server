5885e499aa8db4f3070dcf124382023ce5adf640
Raju Talukder <63764709+Mostafaarju@users.noreply.github.com>
2021-05-20 08:31:41 +0200

    My Server Code

---
+++ index.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require('dotenv').config();
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2o4cg.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
+const port = 5000;
+
+app.use(bodyParser.json());
+app.use(cors());
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+client.connect(err => {
+  const productsCollection = client.db('emaJohnStore').collection('products');
+  const ordersCollection = client.db('emaJohnStore').collection('orders');
+
+  app.post('/addProduct', (req, res) => {
+    const products = req.body;
+    productsCollection.insertOne(products).then(result => {
+      res.send(result.insertedCount);
+    });
+  });
+
+  app.get('/products', (req, res) => {
+    productsCollection.find({}).toArray((err, documents) => {
+      res.send(documents);
+    });
+  });
+
+  app.get('/product/:key', (req, res) => {
+    productsCollection
+      .find({ key: req.params.key })
+      .toArray((err, documents) => {
+        res.send(documents[0]);
+      });
+  });
+
+  app.post('/productByKeys', (req, res) => {
+    const productKeys = req.body;
+    productsCollection
+      .find({ key: { $in: productKeys } })
+      .toArray((err, documents) => {
+        res.send(documents);
+      });
+  });
+
+  app.post('/addOrder', (req, res) => {
+    const order = req.body;
+    ordersCollection.insertOne(order).then(result => {
+      res.send(result.insertedCount > 0);
+    });
+  });
+});
+
+app.listen(port);
